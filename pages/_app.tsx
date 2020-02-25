0456db8ec2e75de21361e75baa3c84b994ae5916
jaewonYoon <21300492@handong.edu>
2020-02-25 23:45:13 +0900

    config, add dependencies, _app.tsx skeleton codes

---
+++ pages/_app.tsx
@@ -0,0 +1,41 @@
+import App,{ AppContext } from 'next/app';
+import { isServer } from '../lib/env'; 
+import React, { ReactNode } from 'react';
+
+interface InitialProps {
+  isServer: boolean;
+}
+
+class MyMobxApp extends App<InitialProps>{
+  static async getInitalProps({
+    Component,
+    ctx
+  }: AppContext): Promise<{
+      pageProps: {};
+      isServer: boolean;
+    }>{
+      let pageProps = {};
+      const { req, res, pathname, query, asPath} = ctx;
+
+    if(Component.getInitialProps){
+      try{
+        pageProps = await Component.getInitialProps(ctx);
+      } catch(e) {
+        (pageProps as any).error = e; 
+      }
+    } 
+
+    return { pageProps, isServer }; 
+
+  }
+  render(): JSX.Element {
+    const { Component, pageProps, isServer, ...rest} = this.props;
+    return(
+      <>
+        Hello MyMobxApp 
+        <Component {...pageProps}/>  
+      </>
+    )
+  }  
+}
+export default MyMobxApp; 
